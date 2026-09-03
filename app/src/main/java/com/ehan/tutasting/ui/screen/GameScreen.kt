8528557e7877171ebd510459fcbc0af06e42edf0
AnandaRehan <anandarehan071@gmail.com>
2026-09-03 21:35:46 +0700

    fix: 6

--- app/src/main/java/com/ehan/tutasting/ui/screen/GameScreen.kt
+++ app/src/main/java/com/ehan/tutasting/ui/screen/GameScreen.kt
@@ -50,7 +50,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ehan.tutasting.ShowMessage
-import com.ehan.tutasting.model.Piilhan
+import com.ehan.tutasting.model.Pilihan
 
 @Composable
 fun GameScreen(
