01e1a012fd0be31984579968ab967e8eb3fe1a36
AnandaRehan <anandarehan071@gmail.com>
2026-09-03 21:24:13 +0700

    fix: 4

--- app/src/main/java/com/ehan/tutasting/model/Models.kt
+++ app/src/main/java/com/ehan/tutasting/model/Models.kt
@@ -17,7 +17,7 @@ enum class ScreenPhase() : Parcelable {
 
 @Parcelize
 enum class PlayerGame(
-    val name: String
+    val displayName: String
 ) : Parcelable {
     PLAYER1("PLAYER 1"),
     PLAYER2("PLAYER 2");
@@ -25,12 +25,12 @@ enum class PlayerGame(
 
 @Parcelize
 enum class Pilihan(
-    val name: String,
+    val displayName: String,
     val code: String
 ) : Parcelable {
     BATU("BATU", "A"),
     KERTAS("KERTAS", "B"),
     GUNTING("GUNTING", "C");
 
-    fun firstWin(a: Pilihan, b: Pilihan): Boolean = if (p1 == Pilihan.BATU && p2 == Pilihan.GUNTING) || (p1 == Pilihan.KERTAS && p2 == Pilihan.BATU) || (p1 == Pilihan.GUNTING && p2 == Pilihan.KERTAS) { true } else { false }
+    fun firstWin(a: Pilihan, b: Pilihan): Boolean = if ((p1 == Pilihan.BATU && p2 == Pilihan.GUNTING) || (p1 == Pilihan.KERTAS && p2 == Pilihan.BATU) || (p1 == Pilihan.GUNTING && p2 == Pilihan.KERTAS)) { true } else { false }
 }
